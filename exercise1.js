f08adc99b75d029cd0a129431ea2acede8298d9e
Dhymas Julyan Riyanto <32106219+dhymasriyanto@users.noreply.github.com>
2021-04-12 22:37:51 +0700

    excercise1 update

---
+++ exercise1.js
@@ -0,0 +1,28 @@
+const game = {
+    'suspects' : [
+        {
+            name: "Rusty",
+            color: "orange"
+        },
+        {
+            name: "Miss Scarlet",
+            color:"red"
+        }
+    ]
+}
+
+
+function loopThis() {
+  for (let i = 0; i < game.suspects.length; i++) {
+      const element = game.suspects[i]
+  } 
+}
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+loopThis()
