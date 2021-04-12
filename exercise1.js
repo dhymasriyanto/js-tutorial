5ae15b2d1d2bf6c8b3a83fa821f2aab819a7fe6b
Dhymas Julyan Riyanto <32106219+dhymasriyanto@users.noreply.github.com>
2021-04-12 22:41:33 +0700

    update excercise1.js

--- exercise1.js
+++ exercise1.js
@@ -1,28 +1,28 @@
 const game = {
-    'suspects' : [
-        {
-            name: "Rusty",
-            color: "orange"
-        },
-        {
-            name: "Miss Scarlet",
-            color:"red"
-        }
-    ]
-}
-
+  suspects: [
+    {
+      name: "Rusty",
+      color: "orange",
+    },
+    {
+      name: "Miss Scarlet",
+      color: "red",
+    },
+  ],
+};
 
 function loopThis() {
   for (let i = 0; i < game.suspects.length; i++) {
-      const element = game.suspects[i]
-  } 
+    const element = game.suspects[i];
+  }
 }
 
-for (const key in object) {
+function loopThen() {
+  for (const key in object) {
     if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
-        
+      const element = object[key];
     }
+  }
 }
 
-loopThis()
+loopThis();
